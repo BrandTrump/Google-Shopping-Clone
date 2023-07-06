6e1ece9571eb5affa89e6381af1210fea2eceaab
Brandon Trump <brandontrump101@gmail.com>
2023-07-06 14:06:51 +1200

    feat: created state for the different filter values & imported next navigation to push the search filters with the search route

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,14 +11,23 @@ import {
   SelectItem,
 } from "@tremor/react";
 import Avatar from "react-avatar";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const SORT_BY_MAP = {
+  r: "Default",
+  rv: "By Review",
+  p: "By Price (low to high)",
+  pd: "By Price (high to low)",
+};
 
 function Header() {
-  const SORT_BY_MAP = {
-    r: "Default",
-    rv: "By Review",
-    p: "By Price (low to high)",
-    pd: "By Price (high to low)",
-  };
+  const [pages, setPages] = useState("");
+  const [sortBy, setSortBy] = useState("");
+  const [minPrice, setMinPrice] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
+
+  const router = useRouter();
   return (
     <header className="flex flex-col items-center md:flex-row md:items-start md:space-x-6 px-2 pt-10 pb-5 md:p-10 md:pb-5">
       <Link href="/">
@@ -32,7 +41,22 @@ function Header() {
       </Link>
 
       <div className="w-full md:max-w-2xl">
-        <form action="">
+        <form
+          action={(formData) => {
+            const searchTerm = formData.get("searchTerm");
+
+            if (!formData.get("searchTerm")) return;
+
+            const params = new URLSearchParams();
+
+            if (pages) params.set("pages", pages.toString());
+            if (sortBy) params.set("sort_by", sortBy.toString());
+            if (minPrice) params.set("min_price", minPrice.toString());
+            if (maxPrice) params.set("max_price", maxPrice.toString());
+
+            router.push(`/search/${searchTerm}?${params.toString()}`);
+          }}
+        >
           <div className="flex items-center gap-2 w-full px-4">
             <div className="flex items-center space-x-2 bg-white shadow-xl rounded-full border-0 px-6 py-4 flex-1">
               <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
@@ -47,7 +71,11 @@ function Header() {
           </div>
 
           <div className="grid grid-cols-2 gap-2 p-4 md:grid-cols-4 max-w-lg md:max-w-none mx-auto items-center">
-            <SearchSelect className="min-w-4" placeholder="# of pages">
+            <SearchSelect
+              className="min-w-4"
+              placeholder="# of pages"
+              onValueChange={(value) => setPages(value)}
+            >
               {[...Array(100)].map((_, i) => (
                 <SearchSelectItem key={i} value={(i + 1).toString()}>
                   {(i + 1).toString()} pages
@@ -55,7 +83,11 @@ function Header() {
               ))}
             </SearchSelect>
 
-            <Select className="min-w-4" placeholder="Sort">
+            <Select
+              className="min-w-4"
+              placeholder="Sort"
+              onValueChange={(value) => setSortBy(value)}
+            >
               {Object.entries(SORT_BY_MAP).map(([key, value]) => (
                 <SelectItem key={key} value={key}>
                   {value}
@@ -63,7 +95,11 @@ function Header() {
               ))}
             </Select>
 
-            <SearchSelect className="min-w-4" placeholder="Min Price...">
+            <SearchSelect
+              className="min-w-4"
+              placeholder="Min Price..."
+              onValueChange={(value) => setMinPrice(value)}
+            >
               {["", "100", "250", "500", "750", "900", "1000+"].map((_, i) => (
                 <SearchSelectItem key={i} value={_.toString()}>
                   {i === 0 ? "No Minimum" : `$${_.toString()}`}
@@ -71,7 +107,11 @@ function Header() {
               ))}
             </SearchSelect>
 
-            <SearchSelect className="min-w-4" placeholder="Max Price...">
+            <SearchSelect
+              className="min-w-4"
+              placeholder="Max Price..."
+              onValueChange={(value) => setMaxPrice(value)}
+            >
               {["", "100", "250", "500", "750", "900", "1000+"].map((_, i) => (
                 <SearchSelectItem key={i} value={_.toString()}>
                   {i === 0 ? "No Maximum" : `$${_.toString()}`}
